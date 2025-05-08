788f3a3548aef28d08e9f444dcc3e0dbe7ee0b03
ankit <132849038+ankitredhat@users.noreply.github.com>
2025-05-08 23:28:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,7 @@ def estimate():
     return render_template('index.html', people=people, image_url=image_path)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    
